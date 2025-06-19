cf855ca3a55ba4657eb7747a3b8b5ff87592c5e2
Ray <raytsang21@gmail.com>
2025-06-19 08:18:54 -0700

    added the frontend project

---
+++ schoolregistration-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+     <>
+      <div className='container bg-red-500 mx-auto'>
+           <h1 className='text-4xl text-amber-200'>Vite + React</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
+
